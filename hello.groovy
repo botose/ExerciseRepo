755c513a818075c9942e991f9a52f4f73892ad8c
Elemér Botos <Elemer_Botos@epam.com>
2016-04-17 19:41:14 +0200

    modified the file

--- hello.groovy
+++ hello.groovy
@@ -1,16 +1,11 @@
 Feature: My first feature file
 
-Scenario Outline: I am the first scenario
-	Given that I am looking for something
-	And find it
-	I am able to use it
-
-Scenario Outline: I am the second scenario
-	Givan that I am not looking for anything
+Scenario: I am the first scenario	
+	Given that I am looking for something	
+	And find it	
+	Then I am able to use it
+	
+Scenario: I am the second but modified scenario	
+	Given that I am not looking for anything
 	When I want to do somthing
 	Then I get a point for "<thing>"
-
-	Examples:
-	| something    | _
-	| first thing  | _
-	| second thing | _
