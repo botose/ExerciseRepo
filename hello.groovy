dc7bb23505fe50a60492f33b1d2065a7eddee088
Elemér Botos <Elemer_Botos@epam.com>
2016-04-17 20:24:54 +0200

    hellofirstcommit

--- hello.groovy
+++ hello.groovy
@@ -1,7 +1,11 @@
 Feature: My first feature file
-Scenario Outline: I am the first scenario	Given that I am looking for something
+
+Scenario Outline: I am the first scenario
+	Given that I am looking for something
 	And find it
 	I am able to use it
-Scenario Outline: I am the second niceer scenario	Givan that I am not looking for anything
+	
+Scenario: I am the second niceer scenario
+	Given that I am not looking for anything
 	When I want to do somthing
 	Then I get a point for "<thing>"
