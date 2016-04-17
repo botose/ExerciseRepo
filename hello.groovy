b293bcb1a17f9b4cd62bbd3966c52078bb430b6f
Elemér Botos <Elemer_Botos@epam.com>
2016-04-17 20:36:59 +0200

    adding another file

--- hello.groovy
+++ hello.groovy
@@ -3,9 +3,11 @@ This is the description of the first feature file
 
 Scenario Outline: I am the first and best scenario
 	Given that I am looking for something
-	And find it	I am able to use it
-	
+	And find it
+	I am able to use it
+
 Scenario Outline: I am the second niceer scenario
-	Given that I am not looking for anything
+	Givan that I am not looking for anything
 	When I want to do somthing
 	Then I get a point for "<thing>"
+
---
+++ world.groovy
@@ -0,0 +1,13 @@
+Feature: y first feature file
+This is the description of the first feature file
+
+Scenario Outline: I a the first and best scenario
+	Given that I a looking for soething
+	And find it
+	I a able to use it
+
+Scenario Outline: I a the second niceer scenario
+	Givan that I a not looking for anything
+	When I want to do sothing
+	Then I get a point for "<thing>"
+
