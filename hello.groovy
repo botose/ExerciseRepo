94c8c68c88e5a6eeddd03ef597927dc2e7813d34
Elemér Botos <Elemer_Botos@epam.com>
2016-04-17 20:33:20 +0200

    fixing end of the lines

--- hello.groovy
+++ hello.groovy
@@ -1,3 +1,11 @@
-Feature: My first feature fileThis is the description of the first feature file
-Scenario Outline: I am the first and best scenario	Given that I am looking for something	And find it	I am able to use it
-Scenario Outline: I am the second niceer scenario	Givan that I am not looking for anything	When I want to do somthing	Then I get a point for "<thing>"
+Feature: My first feature file
+This is the description of the first feature file
+
+Scenario Outline: I am the first and best scenario
+	Given that I am looking for something
+	And find it	I am able to use it
+	
+Scenario Outline: I am the second niceer scenario
+	Given that I am not looking for anything
+	When I want to do somthing
+	Then I get a point for "<thing>"
