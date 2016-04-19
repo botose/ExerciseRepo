b5e52ecf2e31f03a12a0a1523bb945c3cdbb0617
Elemér Botos <Elemer_Botos@epam.com>
2016-04-20 00:35:13 +0200

    asd

--- hello.groovy
+++ hello.groovy
@@ -4,10 +4,10 @@ This is the description of the first feature file
 Scenario Outline: I am the first and best scenario
 	Given that I am looking for something
 	And find it
-	I am able to use it
+	And I am able to use it
 
 Scenario Outline: I am the second niceer scenario
-	Givan that I am not looking for anything
+	Given that I am not looking for anything
 	When I want to do somthing
 	Then I get a point for "<thing>"
 
--- world.groovy
+++ world.groovy
@@ -4,7 +4,7 @@ This is the description of the first feature file
 Scenario Outline: I a the first and best scenario
 	Given that I a looking for soething
 	And find it
-	I a able to use it
+	I A able to use it
 
 Scenario Outline: I a the second niceer scenario
 	Givan that I a not looking for anything
@@ -13,4 +13,5 @@ Scenario Outline: I a the second niceer scenario
 
 Scenario: Third scenario
 	Then I run
+	And I won't stop
 
